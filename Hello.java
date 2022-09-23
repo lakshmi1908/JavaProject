4502e050acca4376fbb6215f2884758d739cbb7c
lakshmi1908 <112061000+lakshmi1908@users.noreply.github.com>
2022-09-23 15:47:55 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello, Mock Assessment!"); 
+        System.out.println("Hello, Term2 Mock Assessment"); 
     }
 }
